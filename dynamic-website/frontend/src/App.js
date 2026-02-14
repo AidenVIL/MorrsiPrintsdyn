c2db83dbabdb132f448474b4410ffb9f3a643fc4
Aiden Viljoen <aidenmviljoen@gmail.ocm>
2026-02-14 21:46:48 +0000

    Move App.js to src folder for React build

---
+++ dynamic-website/frontend/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import LandingPage from '../pages/LandingPage';
+import AuthPage from '../pages/AuthPage';
+import AccountPage from '../pages/AccountPage';
+
+function App() {
+  return (
+    <Router>
+      <nav style={{ display: 'flex', gap: '1rem', padding: '1rem', borderBottom: '1px solid #ccc' }}>
+        <Link to="/">Home</Link>
+        <Link to="/auth">Login/Signup</Link>
+        <Link to="/account">Account</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/auth" element={<AuthPage />} />
+        <Route path="/account" element={<AccountPage />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
